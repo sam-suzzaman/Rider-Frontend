d05495a6bb4622756b069d0b1535dbe55b6ccda7
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-06-13 14:17:38 +0600

    add basic sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from "./Components/Shared/Footer/Footer";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 import HomePage from "./Pages/HomePage/HomePage";
 import SignupPage from "./Pages/AuthenticationPages/SignupPage/SignupPage";
+import SigninPage from "./Pages/AuthenticationPages/SigninPage/SigninPage";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Navbar />
             <HomePage />
             <SignupPage />
+            <SigninPage />
             <Footer />
         </>
     );
---
+++ src/Pages/AuthenticationPages/SigninPage/SigninPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Signin.css";
+
+const SigninPage = () => {
+    return <div>basic sign in page</div>;
+};
+
+export default SigninPage;
